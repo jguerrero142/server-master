223c738e7df1c7addb8d928dc2ef94e82b702bf5
jguerrero142 <julgueover.05@gmail.com>
2021-10-18 22:28:16 -0500

    reserva

--- Angular/server-master/app_store (1).sql
+++ Angular/server-master/app_store (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 11-10-2021 a las 02:03:34
+-- Tiempo de generación: 19-10-2021 a las 03:27:33
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -124,31 +124,91 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `id_user` int(11) NOT NULL,
   `valor` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `estado_ped` tinyint(1) DEFAULT '1',
+  `value_pedido` tinyint(1) DEFAULT '1',
+  `servicio` tinyint(1) DEFAULT '0',
+  `estado_valor` int(11) DEFAULT '1',
+  `pedido_estado` int(11) DEFAULT '1',
   PRIMARY KEY (`id`),
-  KEY `id_user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  KEY `id_user` (`id_user`),
+  KEY `estado_valor` (`estado_valor`),
+  KEY `pedido_estado` (`pedido_estado`)
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `pedido`
 --
 
-INSERT INTO `pedido` (`id`, `id_user`, `valor`, `created_at`, `estado_ped`) VALUES
-(1, 67, 10000, '2021-10-11 00:27:07', 0),
-(2, 68, 10000, '2021-10-11 00:27:59', 0),
-(3, 68, 10000, '2021-10-11 00:33:19', 0),
-(4, 68, 10000, '2021-10-11 00:34:26', 0),
-(5, 67, 10000, '2021-10-11 00:34:59', 0),
-(6, 67, 15000, '2021-10-11 00:35:06', 0),
-(7, 67, 7000, '2021-10-11 00:35:18', 0),
-(8, 67, 23000, '2021-10-11 00:35:37', 0),
-(9, 67, 20000, '2021-10-11 01:16:09', 0),
-(10, 67, 10000, '2021-10-11 01:24:49', 0),
-(11, 67, 15000, '2021-10-11 01:25:21', 0),
-(12, 67, 10000, '2021-10-11 01:27:01', 0),
-(13, 67, 10000, '2021-10-11 01:39:48', 0),
-(14, 67, 15000, '2021-10-11 01:42:16', 0),
-(15, 67, 10000, '2021-10-11 01:47:49', 0);
+INSERT INTO `pedido` (`id`, `id_user`, `valor`, `created_at`, `value_pedido`, `servicio`, `estado_valor`, `pedido_estado`) VALUES
+(76, 67, 10000, '2021-10-18 15:27:33', 0, 0, 1, 1),
+(77, 68, 15000, '2021-10-18 15:28:54', 0, 0, 1, 1),
+(78, 67, 20000, '2021-10-18 15:29:14', 0, 0, 1, 1),
+(79, 68, 15000, '2021-10-18 15:29:24', 0, 0, 1, 1),
+(80, 67, 15000, '2021-10-18 18:32:32', 0, 0, 1, 1),
+(81, 67, 20000, '2021-10-18 23:13:26', 0, 0, 1, 1),
+(82, 67, 10000, '2021-10-18 23:13:47', 0, 0, 1, 1),
+(83, 67, 15000, '2021-10-18 23:26:34', 0, 0, 1, 1),
+(84, 67, 10000, '2021-10-18 23:52:52', 0, 0, 1, 1),
+(85, 67, 15000, '2021-10-19 01:47:20', 0, 0, 1, 1),
+(86, 67, 20000, '2021-10-19 01:50:11', 0, 0, 1, 1),
+(87, 67, 20000, '2021-10-19 01:50:34', 0, 1, 1, 1),
+(88, 67, 32000, '2021-10-19 01:51:05', 0, 0, 1, 1),
+(89, 67, 15000, '2021-10-19 01:53:08', 0, 0, 1, 1),
+(90, 67, 15000, '2021-10-19 01:57:39', 0, 0, 1, 1),
+(91, 67, 15000, '2021-10-19 03:05:04', 0, 0, 1, 1),
+(92, 67, 15000, '2021-10-19 03:05:29', 0, 1, 1, 1),
+(93, 67, 4000, '2021-10-19 03:05:56', 0, 0, 1, 1),
+(94, 67, 20000, '2021-10-19 03:10:00', 0, 0, 1, 1),
+(95, 67, 15000, '2021-10-19 03:10:10', 0, 0, 1, 1),
+(96, 67, 15000, '2021-10-19 03:15:18', 0, 0, 1, 1),
+(97, 67, 15000, '2021-10-19 03:16:37', 0, 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_estado`
+--
+
+DROP TABLE IF EXISTS `pedido_estado`;
+CREATE TABLE IF NOT EXISTS `pedido_estado` (
+  `id_estado` int(11) NOT NULL AUTO_INCREMENT,
+  `name_estado` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `pedido_estado`
+--
+
+INSERT INTO `pedido_estado` (`id_estado`, `name_estado`, `create_at`) VALUES
+(1, 'Creada', '2021-10-18 15:27:05'),
+(2, 'Proceso', '2021-10-18 15:27:05'),
+(3, 'Despachada', '2021-10-18 15:27:05'),
+(4, 'Finalizada', '2021-10-18 15:27:05'),
+(5, 'Contabilizada', '2021-10-18 15:27:05');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_est_valor`
+--
+
+DROP TABLE IF EXISTS `pedido_est_valor`;
+CREATE TABLE IF NOT EXISTS `pedido_est_valor` (
+  `id_estado` int(11) NOT NULL AUTO_INCREMENT,
+  `name_estado` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `pedido_est_valor`
+--
+
+INSERT INTO `pedido_est_valor` (`id_estado`, `name_estado`, `create_at`) VALUES
+(1, 'Credito', '2021-10-18 15:09:04'),
+(2, 'Debito', '2021-10-18 15:09:04'),
+(3, 'Bono', '2021-10-18 15:09:04');
 
 -- --------------------------------------------------------
 
@@ -185,7 +245,7 @@ INSERT INTO `producto` (`id`, `name`, `valor`, `create_at`, `image`, `descripcio
 (15, 'bunuelo', 1000, '2021-09-26 19:03:57', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso bunuelo', 2, 0, 1),
 (16, 'cono de nucita', 1000, '2021-09-26 19:04:53', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso cono de nucita', 5, 0, 1),
 (17, 'paleta tentacion', 1000, '2021-09-26 19:05:23', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso paleta tentacion', 5, 1, 1),
-(18, 'paleta de agua', 1000, '2021-09-26 19:05:56', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso paleta agua', 5, 0, 1),
+(18, 'paleta de agua', 1000, '2021-09-26 19:05:56', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso paleta agua', 5, 1, 1),
 (19, 'bunuelo', 1000, '2021-09-26 19:03:57', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso bunuelo', 2, 0, 1),
 (20, 'bunuelo', 1000, '2021-09-26 19:03:57', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso bunuelo', 2, 0, 1),
 (21, 'bunuelo', 1000, '2021-09-26 19:03:57', 'https://depor.com/resizer/D6DbtUwCx7Pw7ZfvzO0YZGDBUhM=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KI72CKUOHBF4RLJO7EBCR75SKA.jpg', 'Delicioso bunuelo', 2, 0, 1),
@@ -241,65 +301,94 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `id_ticket` int(11) NOT NULL AUTO_INCREMENT,
   `user_ticket` int(11) NOT NULL,
   `producto` int(11) NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `id_pedido` int(11) DEFAULT NULL,
   `estado` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id_ticket`),
   KEY `producto` (`producto`),
   KEY `user_ticket` (`user_ticket`),
   KEY `id_pedido` (`id_pedido`)
-) ENGINE=InnoDB AUTO_INCREMENT=275 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=420 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `ticket`
 --
 
 INSERT INTO `ticket` (`id_ticket`, `user_ticket`, `producto`, `create_at`, `id_pedido`, `estado`) VALUES
-(215, 67, 11, '2021-10-11 00:14:55', 5, 0),
-(216, 67, 10, '2021-10-11 00:14:56', 5, 0),
-(217, 67, 9, '2021-10-11 00:35:03', 6, 0),
-(218, 67, 10, '2021-10-11 00:35:03', 6, 0),
-(219, 67, 11, '2021-10-11 00:35:04', 6, 0),
-(220, 67, 9, '2021-10-11 00:35:13', 7, 0),
-(221, 67, 15, '2021-10-11 00:35:16', 7, 0),
-(222, 67, 14, '2021-10-11 00:35:16', 7, 0),
-(223, 67, 13, '2021-10-11 00:35:24', 8, 0),
-(224, 67, 20, '2021-10-11 00:35:29', 8, 0),
-(225, 67, 22, '2021-10-11 00:35:31', 8, 0),
-(226, 67, 24, '2021-10-11 00:35:31', 8, 0),
-(227, 67, 24, '2021-10-11 00:35:32', 8, 0),
-(228, 67, 24, '2021-10-11 00:35:32', 8, 0),
-(229, 67, 24, '2021-10-11 00:35:32', 8, 0),
-(230, 67, 24, '2021-10-11 00:35:32', 8, 0),
-(231, 67, 24, '2021-10-11 00:35:32', 8, 0),
-(232, 67, 24, '2021-10-11 00:35:33', 8, 0),
-(233, 67, 24, '2021-10-11 00:35:33', 8, 0),
-(234, 67, 24, '2021-10-11 00:35:33', 8, 0),
-(235, 67, 24, '2021-10-11 00:35:33', 8, 0),
-(236, 67, 24, '2021-10-11 00:35:34', 8, 0),
-(237, 67, 24, '2021-10-11 00:35:34', 8, 0),
-(238, 67, 22, '2021-10-11 00:35:35', 8, 0),
-(239, 67, 22, '2021-10-11 00:35:35', 8, 0),
-(240, 67, 22, '2021-10-11 00:35:36', 8, 0),
-(241, 67, 22, '2021-10-11 00:35:36', 8, 0),
-(242, 67, 10, '2021-10-11 01:16:06', 9, 0),
-(243, 67, 11, '2021-10-11 01:16:07', 9, 0),
-(244, 67, 10, '2021-10-11 01:16:07', 9, 0),
-(245, 67, 9, '2021-10-11 01:16:08', 9, 0),
-(249, 67, 9, '2021-10-11 01:24:17', 10, 0),
-(250, 67, 10, '2021-10-11 01:24:18', 10, 0),
-(251, 67, 11, '2021-10-11 01:25:15', 11, 0),
-(252, 67, 10, '2021-10-11 01:25:17', 11, 0),
-(253, 67, 10, '2021-10-11 01:25:18', 11, 0),
-(255, 67, 10, '2021-10-11 01:25:22', 12, 0),
-(256, 67, 9, '2021-10-11 01:25:22', 12, 0),
-(265, 67, 10, '2021-10-11 01:39:45', 13, 0),
-(266, 67, 9, '2021-10-11 01:39:46', 13, 0),
-(270, 67, 10, '2021-10-11 01:42:12', 14, 0),
-(271, 67, 9, '2021-10-11 01:42:13', 14, 0),
-(272, 67, 10, '2021-10-11 01:42:14', 14, 0),
-(273, 67, 10, '2021-10-11 01:47:39', 15, 0),
-(274, 67, 11, '2021-10-11 01:47:40', 15, 0);
+(346, 67, 9, '2021-10-18 15:27:34', 76, 1),
+(347, 67, 9, '2021-10-18 15:27:34', 76, 1),
+(348, 68, 9, '2021-10-18 15:28:54', 77, 1),
+(349, 68, 9, '2021-10-18 15:28:54', 77, 1),
+(350, 68, 9, '2021-10-18 15:28:54', 77, 1),
+(351, 67, 10, '2021-10-18 15:29:14', 78, 1),
+(352, 67, 9, '2021-10-18 15:29:14', 78, 1),
+(353, 67, 9, '2021-10-18 15:29:14', 78, 1),
+(354, 67, 9, '2021-10-18 15:29:15', 78, 1),
+(355, 68, 10, '2021-10-18 15:29:24', 79, 1),
+(356, 68, 10, '2021-10-18 15:29:24', 79, 1),
+(357, 68, 10, '2021-10-18 15:29:24', 79, 1),
+(358, 67, 9, '2021-10-18 18:32:32', 80, 1),
+(359, 67, 10, '2021-10-18 18:32:32', 80, 1),
+(360, 67, 10, '2021-10-18 18:32:32', 80, 1),
+(361, 67, 9, '2021-10-18 23:13:26', 81, 1),
+(362, 67, 10, '2021-10-18 23:13:26', 81, 1),
+(363, 67, 10, '2021-10-18 23:13:26', 81, 1),
+(364, 67, 10, '2021-10-18 23:13:26', 81, 1),
+(365, 67, 10, '2021-10-18 23:13:47', 82, 1),
+(366, 67, 10, '2021-10-18 23:13:47', 82, 1),
+(367, 67, 10, '2021-10-18 23:26:34', 83, 1),
+(368, 67, 10, '2021-10-18 23:26:34', 83, 1),
+(369, 67, 10, '2021-10-18 23:26:34', 83, 1),
+(370, 67, 9, '2021-10-18 23:52:52', 84, 1),
+(371, 67, 10, '2021-10-18 23:52:52', 84, 1),
+(372, 67, 9, '2021-10-19 01:47:20', 85, 1),
+(373, 67, 10, '2021-10-19 01:47:20', 85, 1),
+(374, 67, 10, '2021-10-19 01:47:20', 85, 1),
+(375, 67, 10, '2021-10-19 01:50:11', 86, 1),
+(376, 67, 10, '2021-10-19 01:50:11', 86, 1),
+(377, 67, 9, '2021-10-19 01:50:11', 86, 1),
+(378, 67, 9, '2021-10-19 01:50:11', 86, 1),
+(379, 67, 10, '2021-10-19 01:50:34', 87, 1),
+(380, 67, 9, '2021-10-19 01:50:34', 87, 1),
+(381, 67, 13, '2021-10-19 01:50:34', 87, 1),
+(382, 67, 12, '2021-10-19 01:50:34', 87, 1),
+(383, 67, 9, '2021-10-19 01:51:05', 88, 1),
+(384, 67, 10, '2021-10-19 01:51:05', 88, 1),
+(385, 67, 10, '2021-10-19 01:51:05', 88, 1),
+(386, 67, 13, '2021-10-19 01:51:05', 88, 1),
+(387, 67, 12, '2021-10-19 01:51:05', 88, 1),
+(388, 67, 17, '2021-10-19 01:51:05', 88, 1),
+(389, 67, 17, '2021-10-19 01:51:05', 88, 1),
+(390, 67, 12, '2021-10-19 01:51:05', 88, 1),
+(391, 67, 10, '2021-10-19 01:53:08', 89, 1),
+(392, 67, 9, '2021-10-19 01:53:08', 89, 1),
+(393, 67, 9, '2021-10-19 01:53:08', 89, 1),
+(394, 67, 10, '2021-10-19 01:57:39', 90, 1),
+(395, 67, 10, '2021-10-19 01:57:39', 90, 1),
+(396, 67, 10, '2021-10-19 01:57:39', 90, 1),
+(397, 67, 9, '2021-10-19 03:05:04', 91, 1),
+(398, 67, 9, '2021-10-19 03:05:04', 91, 1),
+(399, 67, 9, '2021-10-19 03:05:04', 91, 1),
+(400, 67, 10, '2021-10-19 03:05:29', 92, 1),
+(401, 67, 10, '2021-10-19 03:05:29', 92, 1),
+(402, 67, 10, '2021-10-19 03:05:29', 92, 1),
+(403, 67, 14, '2021-10-19 03:05:57', 93, 1),
+(404, 67, 14, '2021-10-19 03:05:57', 93, 1),
+(405, 67, 14, '2021-10-19 03:05:57', 93, 1),
+(406, 67, 14, '2021-10-19 03:05:57', 93, 1),
+(407, 67, 10, '2021-10-19 03:10:00', 94, 1),
+(408, 67, 10, '2021-10-19 03:10:00', 94, 1),
+(409, 67, 10, '2021-10-19 03:10:00', 94, 1),
+(410, 67, 10, '2021-10-19 03:10:00', 94, 1),
+(411, 67, 9, '2021-10-19 03:10:11', 95, 1),
+(412, 67, 9, '2021-10-19 03:10:11', 95, 1),
+(413, 67, 9, '2021-10-19 03:10:11', 95, 1),
+(414, 67, 10, '2021-10-19 03:15:18', 96, 1),
+(415, 67, 10, '2021-10-19 03:15:18', 96, 1),
+(416, 67, 10, '2021-10-19 03:15:18', 96, 1),
+(417, 67, 9, '2021-10-19 03:16:37', 97, 1),
+(418, 67, 9, '2021-10-19 03:16:37', 97, 1),
+(419, 67, 9, '2021-10-19 03:16:37', 97, 1);
 
 -- --------------------------------------------------------
 
@@ -352,15 +441,27 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `sub` (`sub`),
   KEY `id_empresa` (`id_empresa`),
   KEY `role` (`role`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`id_user`, `sub`, `name`, `picture`, `email`, `create_at`, `given_name`, `family_name`, `nickname`, `locale`, `updated_at`, `email_verified`, `role`, `id_empresa`) VALUES
-(67, 'google-oauth2|112703936879554720334', 'Julian Guerrero', 'https://lh3.googleusercontent.com/a-/AOh14GhvDUEyiG-04sVHxOvp4dqZPiwHmokd7K40NaDAAA=s96-c', 'julgueover.05@gmail.com', '2021-09-23 00:22:35', 'Julian', 'Guerrero', 'julgueover.05', 'es', '2021-10-10T22:18:06.880Z', '1', 3, 1),
-(68, 'google-oauth2|115554611911654128336', 'Andrew Cast', 'https://lh3.googleusercontent.com/a-/AOh14GjHX6L2gWJzcIUeB7-GQIxiCMs9xgHMBMUze5S3=s96-c', 'andrewcast.0707@gmail.com', '2021-10-06 01:27:29', 'Andrew', 'Cast', 'andrewcast.0707', 'es', '2021-10-06T01:27:54.099Z', '1', 3, 1);
+(67, 'google-oauth2|112703936879554720334', 'Julian Guerrero', 'https://lh3.googleusercontent.com/a-/AOh14GhvDUEyiG-04sVHxOvp4dqZPiwHmokd7K40NaDAAA=s96-c', 'julgueover.05@gmail.com', '2021-09-23 00:22:35', 'Julian', 'Guerrero', 'julgueover.05', 'es', '2021-10-19T01:24:41.784Z', '1', 3, 1),
+(68, 'google-oauth2|115554611911654128336', 'Andrew Cast', 'https://lh3.googleusercontent.com/a-/AOh14GjHX6L2gWJzcIUeB7-GQIxiCMs9xgHMBMUze5S3=s96-c', 'andrewcast.0707@gmail.com', '2021-10-06 01:27:29', 'Andrew', 'Cast', 'andrewcast.0707', 'es', '2021-10-18T00:57:35.982Z', '1', 3, 1),
+(69, 'google-oauth2|112703936879554720345', NULL, NULL, NULL, '2021-10-18 21:32:59', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(71, 'google-oauth2|112703936879554720322', NULL, NULL, NULL, '2021-10-18 21:33:38', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(72, 'google-oauth2|112703936879554720321', NULL, NULL, NULL, '2021-10-18 21:39:51', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(75, 'google-oauth2|112703936879554720318', NULL, NULL, NULL, '2021-10-18 21:41:40', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(76, 'google-oauth2|112703936879554720316', 'juscsliasssggns', NULL, NULL, '2021-10-18 21:42:55', NULL, NULL, NULL, 'Cali', NULL, NULL, 3, 1),
+(77, 'google-oauth2|112703936879554720313', NULL, NULL, NULL, '2021-10-18 22:12:50', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(78, 'google-oauth2|1127039368795547203111', NULL, NULL, NULL, '2021-10-18 22:19:14', NULL, NULL, NULL, NULL, NULL, NULL, 3, 1),
+(79, 'google-oauth2|112703936879554720311122', 'juscsliasssggns', NULL, NULL, '2021-10-18 22:20:04', NULL, NULL, NULL, 'Cali', NULL, NULL, 3, 1),
+(80, 'google-oauth2|1127039368795547203111e323', 'jugns', NULL, NULL, '2021-10-18 22:20:17', NULL, NULL, NULL, 'Cali', NULL, NULL, 3, 1),
+(81, 'google-oauth2|1127039368795547203111sdasda', 'jugnddds', NULL, NULL, '2021-10-18 22:21:14', NULL, NULL, NULL, 'Cali', NULL, NULL, 3, 1),
+(82, 'google-oauth2|1127039368795ss547203111sdasda', 'jugssssdds', NULL, NULL, '2021-10-18 22:21:21', NULL, NULL, NULL, 'Cali', NULL, NULL, 3, 1),
+(83, 'google-oauth2|1127039368795ss547203ss111sdasda', 'jugssssdds', NULL, NULL, '2021-10-18 22:22:28', NULL, NULL, NULL, 'Cacccli', NULL, NULL, 3, 1);
 
 --
 -- Restricciones para tablas volcadas
@@ -377,7 +478,9 @@ ALTER TABLE `factura`
 -- Filtros para la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`estado_valor`) REFERENCES `pedido_est_valor` (`id_estado`),
+  ADD CONSTRAINT `pedido_ibfk_3` FOREIGN KEY (`pedido_estado`) REFERENCES `pedido_estado` (`id_estado`);
 
 --
 -- Filtros para la tabla `producto`
